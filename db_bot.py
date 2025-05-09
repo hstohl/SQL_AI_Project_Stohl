4cf7230f6be8940add86afde04b2d011ab1ef5d1
Hudson Stohl <hrstohl02@gmail.com>
2025-05-09 14:53:14 -0600

    init

---
+++ db_bot.py
@@ -0,0 +1,35 @@
+import json
+from openai import OpenAI
+import os
+import sqlite3
+from time import time
+
+fdir = os.path.dirname(__file__)
+def getPath(fname):
+    return os.path.join(fdir, fname)
+
+# SQLITE
+sqliteDbPath = getPath("aidb.sqlite")
+setupSqlPath = getPath("setup.sql")
+setupSqlDataPath = getPath("setupData.sql")
+
+# Erase previous db
+if os.path.exists(sqliteDbPath):
+    os.remove(sqliteDbPath)
+
+sqliteCon = sqlite3.connect(sqliteDbPath) # create new db
+sqliteCursor = sqliteCon.cursor()
+with (
+        open(setupSqlPath) as setupSqlFile,
+        open(setupSqlDataPath) as setupSqlDataFile
+    ):
+
+    setupSqlScript = setupSqlFile.read()
+    setupSQlDataScript = setupSqlDataFile.read()
+
+sqliteCursor.executescript(setupSqlScript) # setup tables and keys
+sqliteCursor.executescript(setupSQlDataScript) # setup tables and keys
+
+def runSql(query):
+    result = sqliteCursor.execute(query).fetchall()
+    return result
---
+++ setup.sql
@@ -0,0 +1,61 @@
+create table player (
+    id integer primary key,
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    birth_date date not null,
+    bat_side varchar(1) not null,
+    throw_arm varchar(1) not null,
+)
+
+create table team (
+    id integer primary key,
+    name varchar(20) not null,
+    year integer not null,
+)
+
+create table player_team (
+    player_id integer not null,
+    team_id integer not null,
+    foreign key (player_id) references player(id),
+    foreign key (team_id) references team(id)
+)
+
+create table position (
+    id integer primary key,
+    position varchar(20) not null,
+)
+
+create table player_position (
+    player_id integer not null,
+    position_id integer not null,
+    foreign key (player_id) references player(id),
+    foreign key (position_id) references position(id)
+)
+
+create table pitching_stats (
+    player_id integer not null,
+    year integer not null,
+    era decimal(3,2) not null,
+    innings_pitched decimal(3,1) not null,
+    wins integer not null,
+    losses integer not null,
+    strikeouts integer not null,
+    foreign key (player_id) references player(id)
+)
+
+create table batting_stats (
+    player_id integer not null,
+    year integer not null,
+    batting_average decimal(1,3) not null,
+    at_bats integer not null,
+    hits integer not null,
+    home_runs integer not null,
+    foreign key (player_id) references player(id)
+)
+
+create table awards (
+    id integer primary key,
+    player_id integer not null,
+    award_name varchar(50) not null,
+    year integer not null,
+)
